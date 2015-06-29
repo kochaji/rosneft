d40ce35d5c0bf5f6e936f26b2d56cf44df7a7ffe
Vadim Kochergin <tamato2008@rambler.ru>
2015-06-29 17:02:53 +0300

    no message

--- _feature.php
+++ _feature.php
@@ -22,7 +22,7 @@
 -->
 
 <div class="feature js-feature-video active">
-	<video preload="meta" class="feature__video hidden" loop muted>
+	<video preload="meta" class="feature__video hidden" loop muted controls>
 		<source src="video/<?= $media['webm'] ?>" type="video/webm; codecs=&quot;vp8, vorbis&quot;">
 		<source src="video/<?= $media['mp4']  ?>"  type="video/mp4; codecs=&quot;avc1.42E01E, mp4a.40.2&quot;">
 		<source src="video/<?= $media['ogv']  ?>"  type="video/ogg; codecs=&quot;theora, vorbis&quot;">
