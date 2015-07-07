6b35c152a171faa4461f6041ab88cf49cb0ed5ef
tamato <tamato2008@rambler.ru>
2015-07-08 01:00:06 +0400

    test

---
+++ _header.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>История нефти</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<meta property="og:type"        content="website" />
+	<meta property="og:url"         content="" />
+	<meta property="og:title"       content="" />
+	<meta property="og:description" content="" />
+	<meta property="og:image" content="" />
+
+	<link href='http://fonts.googleapis.com/css?family=PT+Sans+Caption:400,700&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=PT+Serif&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+
+	<link rel="shortcut icon" href="favicon.ico"></link>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/social-likes/3.0.14/social-likes_flat.css">
+	<link rel="stylesheet" href="css/style.css">
+
+	<script src="//use.typekit.net/dnu0aez.js"></script>
+	<script>try{Typekit.load();}catch(e){}</script>
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script>
+		window.jQuery || document.write('<scr'+'ipt src="assets/js/jquery-1.11.1.min.js"></scr'+'ipt>');
+	</script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.5/waypoints.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/social-likes/3.0.14/social-likes.min.js"></script>
+	<script src="js/imgloader.js"></script>
+	
+
+</head>
+<body class="">
---
+++ test.php
@@ -0,0 +1,82 @@
+<?php require_once '_header.php' ?>
+
+<link rel="stylesheet" href="css/test.css">
+<script src="js/test.js"></script>
+
+<div class="test-screen js-test-screen">
+	<div class="vcenter">
+		<div class="vcentered">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-16 col-md-offset-2">
+						<div class="test js-test in-progress" id="testApp">
+							<div class="test__inner">
+								<div class="test__header row">
+									<h3 class="title test__result__header pull-left for-results">
+										Вы правильно ответили на
+										<span class="h2 title test__result__header__scores js-test-scores">
+											
+										</span>
+										из
+										<span class="test__result__header__total js-test-scores-total">
+											
+										</span>
+										вопросов
+									</h3>
+									<div class="test__header__numbers for-progress">
+										<div class="h1 test__header__numbers__current js-test-current">5</div>
+										/
+										<div class="h2 test__header__numbers__total js-test-total">21</div>
+									</div>
+								</div>
+								<h2 class="h2 title calc__title js-test-question for-progress"></h2>
+								<div class="test__answer__list js-test-answers for-progress">
+									<a href="#%answer%" class="test__answer__list__item js-test-answer" data-answer="%data%">
+										<span class="h5">%answer%</span>
+									</a>
+								</div>
+							</div>
+							<div class="test__results for-results">
+								<div class="test__inner">
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. In fugit porro repellat excepturi, eius tempore, error, minima doloribus laboriosam molestias cupiditate! Ratione id, sed itaque rem quisquam natus non hic.
+								</div>
+							</div>
+							<div class="test__text test__inner for-progress">
+								<div class="js-test-text">
+								</div>
+								<div class="test__btn test__btn--next js-test-next">
+									Следующий вопрос <span>>></span>
+								</div>
+								<div class="test__btn test__btn--to-results js-test-next">
+									Результат >>
+								</div>
+							</div>
+							<div class="test__footer for-results test__inner">
+								<div class="row">
+									<div class="pull-left">
+										<span>Поделиться</span>
+										<div class="social-likes" data-zeroes="yes">
+											<div class="vkontakte"></div>
+											<div class="facebook"></div>
+											<div class="twitter"></div>
+										</div>
+									</div>
+									<div class="pull-right">
+										<a href="#" class="js-test-start">
+											Пройти заново
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+<?php require_once '_support.php' ?>
+<?php require_once '_footer.php' ?>
